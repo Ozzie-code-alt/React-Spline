35f06ccb4b81b25612e6e071eb38b80d2cab0371
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-09-11 23:26:33 +0800

    feat: add spline

---
+++ spline-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/SUBylaKSe-pYliBE/scene.splinecode" 
+      />
+    </main>
+  );
+}
